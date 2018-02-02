7006b3b223b4d4d858c79d27b6da2d091ffd38a8
Hunter <hunter@unity3d.com>
2018-02-01 16:38:03 -0800

    added gravity mult to WallJumpAcademy

--- unity-environment/Assets/ML-Agents/Examples/New Demos/PushBlock/Wall Jump/Scripts/WallJumpAcademy.cs
+++ unity-environment/Assets/ML-Agents/Examples/New Demos/PushBlock/Wall Jump/Scripts/WallJumpAcademy.cs
@@ -11,6 +11,7 @@ public class WallJumpAcademy : Academy {
     public Material goalScoredMaterial; //when a goal is scored the ground will use this material for a few seconds.
     public Material failMaterial; //when fail, the ground will use this material for a few seconds. 
 
+	public float gravityMultiplier; //use ~3 to make things less floaty
 	public float minWallHeight;
     public float maxWallHeight;
 
@@ -21,7 +22,7 @@ public class WallJumpAcademy : Academy {
 	}
 	// Use this for initialization
 	void Start () {
-		
+		Physics.gravity *= gravityMultiplier; //Normally you shouldn't override Start() or Awake() with MLAgents, but Start() isn't used in Academy.cs so this should be ok for now.
 	}
 	
 	// Update is called once per frame
